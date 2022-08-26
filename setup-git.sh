7980259bec603dec2ef71028bb9c1e35b4d6d237
Andrew Leung <andrewleung104@gmail.com>
2022-08-25 21:25:23 -0400

    Don't create key in secrets dir
    This is protected! They'll let me create the file elsewhere thought, right? Right??

--- setup-git.sh
+++ setup-git.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 eval "$(ssh-agent -s)"
-touch /etc/secrets/id_ed25519_copy
-cat /etc/secrets/id_ed25519 >> /etc/secrets/id_ed25519_copy
-echo "" >> /etc/secrets/id_ed25519_copy
-ssh-add /etc/secrets/id_ed25519_copy
+touch gh_ssh_key
+cat /etc/secrets/id_ed25519 >> gh_ssh_key
+echo "" >> gh_ssh_key
+ssh-add gh_ssh_key
 
 GIT=`which git`
 ${GIT} clone git@github.com:andrewjleung/tnd-reviews.git
