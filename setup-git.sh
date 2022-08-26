4c4119c30c0738599d69863031df8b8133a79403
Andrew Leung <andrewleung104@gmail.com>
2022-08-25 21:35:28 -0400

    Try adding GH to list of authorized hosts
    https://stackoverflow.com/a/29380672

--- setup-git.sh
+++ setup-git.sh
@@ -5,6 +5,7 @@ touch gh_ssh_key
 cat /etc/secrets/id_ed25519 >> gh_ssh_key
 echo "" >> gh_ssh_key
 chmod 600 gh_ssh_key
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 ssh-add gh_ssh_key
 
 GIT=`which git`
