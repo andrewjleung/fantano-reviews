dcd5f05c7d0437a16f5f1622b206a4391b0b07c4
Andrew Leung <andrewleung104@gmail.com>
2022-08-25 21:19:10 -0400

    Write to a duplicate key file
    I don't have the permissions to write a trailing newline to the original key file. I could alternatively try to modify those permissions, but we'll see how this goes first.

--- setup-git.sh
+++ setup-git.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 eval "$(ssh-agent -s)"
-echo "" >> /etc/secrets/id_ed25519
-ssh-add /etc/secrets/id_ed25519
+touch /etc/secrets/id_ed25519_copy
+cat /etc/secrets/id_ed25519 >> /etc/secrets/id_ed25519_copy
+echo "" >> /etc/secrets/id_ed25519_copy
+ssh-add /etc/secrets/id_ed25519_copy
 
 GIT=`which git`
 ${GIT} clone git@github.com:andrewjleung/tnd-reviews.git
