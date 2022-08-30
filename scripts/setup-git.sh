e3cd976a8b7df2d993b21457144102ab96452746
Andrew Leung <andrewleung104@gmail.com>
2022-08-30 19:14:07 -0400

    Add public key as well

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -2,17 +2,21 @@
 
 eval "$(ssh-agent -s)"
 
+touch ~/.ssh/id_ed25519
+toucn ~/.ssh/id_ed25519.pub
+cat /etc/secrets/id_ed25519 >> ~/.ssh/id_ed25519
+cat /etc/secrets/id_ed25519.pub >> ~/.ssh/id_ed25519.pub
+
 # Render (hosting service) trims secret files, which causes `ssh-add` to fail
 # since it expects a trailing newline. The `secrets` directory is RO, so we
 # create a new file in the file system we can modify, copy the key over, then
 # add the trailing newline.
-touch gh_ssh_key
-cat /etc/secrets/id_ed25519 >> gh_ssh_key
-echo "" >> gh_ssh_key
+echo "" >> ~/.ssh/id_ed25519
+echo "" >> ~/.ssh/id_ed25519.pub
 
 # `ssh-add` ignores key files that are too permissive. This adds the proper
 # permissions for an SSH key.
-chmod 600 gh_ssh_key
+chmod 600 ~/.ssh/id_ed25519
 
 # The VM doesn't have GitHub as a known host by default.
 # https://serverfault.com/a/1098531
@@ -21,7 +25,7 @@ curl --silent https://api.github.com/meta | \
   >> ~/.ssh/known_hosts
 
 # Finally add the key so that the dataset can be pulled and modified.
-ssh-add gh_ssh_key
+ssh-add ~/.ssh/id_ed25519
 
 GIT=`which git`
 ${GIT} clone git@github.com:andrewjleung/tnd-reviews.git
