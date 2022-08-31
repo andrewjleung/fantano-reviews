74958359a00381bd3b19b42be398db8fc4b0fe76
Andrew Leung <andrewleung104@gmail.com>
2022-08-31 01:25:53 -0400

    Attempt to fix bad substitution of token

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-TOKEN=${cat etc/secrets/gh_pat}
-
 GIT=`which git`
 ${GIT} clone https://github.com/andrewjleung/tnd-reviews.git
 cd tnd-reviews
-${GIT} remote set-url origin https://andrewjleung:$(TOKEN)@github.com/andrewjleung/tnd-reviews.git
+${GIT} remote set-url origin https://andrewjleung:${cat etc/secrets/gh_pat}@github.com/andrewjleung/tnd-reviews.git
 ${GIT} config user.name "Andrew Leung"
 ${GIT} config user.email "andrewleung104@gmail.com"
