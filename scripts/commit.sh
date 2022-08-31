678ad3f1fc678965e6509d694e48b5c59d84b516
Andrew Leung <andrewleung104@gmail.com>
2022-08-30 23:43:59 -0400

    Revert "Re-add github to known hosts?"
    This reverts commit f0773708a860990cd6730e707c811e7f2852ece8.

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,12 +5,6 @@ GIT=`which git`
 
 cd ../tnd-reviews
 
-# The VM doesn't have GitHub as a known host by default.
-# https://serverfault.com/a/1098531
-curl --silent https://api.github.com/meta | \
-  python3 -c 'import json,sys;print(*["github.com " + x for x in json.load(sys.stdin)["ssh_keys"]], sep="\n")' \
-  >> ~/.ssh/known_hosts
-
 ssh -T git@github.com
 
 ${GIT} add reviews.csv all_videos.json && \
