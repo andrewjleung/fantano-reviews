19c1f8208d35823b7ed21cbd7b078b67b5dbc32f
Andrew Leung <andrewleung104@gmail.com>
2022-09-22 16:42:20 -0400

    Pull before making changes; use TS; use different pshb lib

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,13 +5,6 @@ GIT=`which git`
 
 cd ../tnd-reviews
 
-# Pulling before WON'T update the in-memory cache of video IDs used to prevent
-# duplicate videos from being added. This assumes that no changes will be made
-# to the datasets beyond the callback server auto-updating it or a refresh of
-# the ENTIRE dataset. 
-${GIT} stash
-${GIT} pull
-${GIT} stash apply
 ${GIT} add . && \
 ${GIT} commit -m "$message" && \
 ${GIT} push
